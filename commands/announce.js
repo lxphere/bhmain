4b5887775068fd2a23fc63fa41dbef55d1796c2c
lxphere <47260747+lxphere@users.noreply.github.com>
2019-09-19 16:16:10 -0400

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -1,24 +1,24 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-	if (!message.member.roles.get("579601910890233856")) return message.reply("Nope, not today! You need to be an SHR+, nice try though.").catch(() => bot.safeSend(message, module.exports.help.name));
+	if (!message.member.roles.get("623987915281793064")) return message.reply("You must be an MR or above to run this command.").catch(() => bot.safeSend(message, module.exports.help.name));
 		let embed = new Discord.RichEmbed()
-		.setTitle("K | Kauai Announcement")
+		.setTitle("Bean House | Shift")
 		.setColor("#fff993")
 		.setDescription(args.join(" "))
 		.setFooter(`This announcement was made by ${message.author.tag}`, message.author.displayAvatarURL)
  		.setThumbnail("https://cdn.discordapp.com/attachments/438816225909276683/580512978277761025/image0.png");
-		bot.channels.get("623608305222418443").send({embed: embed}).then(() => {
+		bot.channels.get("623994687841370114").send({embed: embed}).then(() => {
 		message.reply("Message sent!");
 	}).catch(() => {
-		message.reply("Something went wrong when announcing, please check my permissions and try again.");
+		message.reply("Something went wrong when sending the shift announcement, please check my error, <@293060399106883584>.");
 	});
 };
 module.exports.help = {
-	name: "announce",
-	usage: "announce <txt>",
+	name: "shift",
+	usage: "shift <txt>",
 	description: "Announcement command, no ping.",
 	longdes: "An announcement command, with no ping.",
-	mentionedperm: "MANAGE_ROLES",
+	mentionedperm: "Session Perm Roles",
 	category: "Utility"
 };
