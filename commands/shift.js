480f8f33951619551ce7f50d71b57c44087ab0f6
lxphere <47260747+lxphere@users.noreply.github.com>
2019-09-20 15:52:32 -0400

    Update shift.js

--- commands/shift.js
+++ commands/shift.js
@@ -4,11 +4,11 @@ module.exports.run = async (bot, message, args) => {
 	let embed = new Discord.RichEmbed()
 		.setTitle("Bean House Shift")
 		.setColor("#63dfff")
-		.setDescription("<:BeanHouseIcon:551253446808961035> | Hello everyone, I am hosting a shift at the cafe! Come on and join me for a luxrious time! :link: https://www.roblox.com/games/3620192724/Bean-House-Cafe")
+		.setDescription("<:BeanHouseIcon:551253446808961035> | Hello everyone, I am hosting a shift at the cafe! Come on and join me for a luxrious time! Link: https://www.roblox.com/games/3620192724/Bean-House-Cafe")
 		.setFooter(`Shift is being hosted by ${message.author.tag}`, message.author.displayAvatarURL)
 	   	.setThumbnail("https://cdn.discordapp.com/attachments/528399681185906736/622802049448542208/1ea15beadf327b617edbe2e5ed5a7f6b.png");
           	bot.channels.get("622531865554845737").send("@here", {embed: embed}).then(() => {
-		message.reply("Sent! Please make sure the next shift is 45 minutes from this current shift.");
+		message.reply("Sent! Please make sure the next shift is **45 minutes** from this current shift.");
 	}).catch(() => {
 		message.reply("Something went wrong when sending the shift announcement, please check my error, <@293060399106883584>.");
 	});
