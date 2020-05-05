30f938699eed8ec4bc1c652e0d01915a0e3be015
lxphere <47260747+lxphere@users.noreply.github.com>
2020-05-05 12:16:36 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -63,7 +63,7 @@ module.exports.run = async (bot, message, args) => {
 					timestamp: new Date(),
 					footer: {
 						icon_url: bot.user.avatarURL,
-						text: `${bot.developers.member.user.tag}`
+						text: `$dev.tag`
 					}
 				}
 			});
