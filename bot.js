6fb1688976fbbdf933a342d89fe8c8f9b0e6ee3e
lxphere <47260747+lxphere@users.noreply.github.com>
2020-05-05 12:14:40 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,8 +4,8 @@ const fs = require("fs");
 module.exports.run = async () => {
 	const client = new Discord.Client({ disableEveryone: false, fetchAllMembers: true });
 	client.commands = new Discord.Collection();
-	const bot = client
-	bot.developers = ["293060399106883584"];
+	const devID = ["293060399106883584"];
+	const dev = client.users.get(devID) || await client.fetchUser(devID);
 
 	fs.readdir("./commands", (err, files) => {
 		if (err) console.log(err);
