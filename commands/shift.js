3d20ae2d96fc1c0257eb596606d1cd36209fdad5
lxphere <47260747+lxphere@users.noreply.github.com>
2020-05-06 11:52:48 -0400

    Update shift.js

--- commands/shift.js
+++ commands/shift.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 	if (!message.member.roles.get("623616153247285259")) return message.reply("You must be an MR or above to run this command.").catch(() => bot.safeSend(message, module.exports.help.name));
+	let timestamp = new Date();
 	let embed = new Discord.RichEmbed()
 		.setTitle("Bean House Shift")
 		.setColor("#faf21b")
@@ -9,7 +10,7 @@ module.exports.run = async (bot, message, args) => {
 	   	.setThumbnail("https://cdn.discordapp.com/attachments/528399681185906736/622802049448542208/1ea15beadf327b617edbe2e5ed5a7f6b.png");
           	bot.channels.get("625464803863035936").send("a", {embed: embed}).then(() => {
 		message.reply("Sent! Please make sure the next shift is **45 minutes** from this current shift.");
-		let timestamp = new Date();
+
 		}).catch(error => bot.channels.get("707607323329429564").send(`An unexpected error has occured. ${message.author.tag} ran the shift command and an unexpected error occured: \n**${error}** \n *Error occured at ${timestamp}*`))
 		message.reply("Error occured; the developer has been sent the error message.");
 };
