092528eeae680f218a5e00da9167d2bf4dbc4aa0
lxphere <47260747+lxphere@users.noreply.github.com>
2020-05-11 15:29:14 -0400

    Update shift.js

--- commands/shift.js
+++ commands/shift.js
@@ -8,8 +8,8 @@ module.exports.run = async (bot, message, args) => {
 		.setColor("#faf21b")
 		.setDescription("<:BeanHouseIcon:551253446808961035> | Hello everyone, I am hosting a shift at the cafe! Come on and join me for a luxurious time! Link: https://www.roblox.com/games/3620192724/Bean-House-Cafe")
 		.setFooter(`This shift is being hosted by ${message.author.tag}`, message.author.displayAvatarURL)
-	   	.setThumbnail("https://cdn.discordapp.com/attachments/528399681185906736/622802049448542208/1ea15beadf327b617edbe2e5ed5a7f6b.png");
-          	bot.channels.get("622531865554845737").send(`${adminRoleObject} *Annoyed you got the ping? Head over to <#429079337132752907> and react with the logo to remove the Session Tag role!`", {embed: embed}).then(() => {
+	   	.setThumbnail(`https://cdn.discordapp.com/attachments/528399681185906736/622802049448542208/1ea15beadf327b617edbe2e5ed5a7f6b.png");
+          	bot.channels.get("622531865554845737").send("${adminRoleObject} *Annoyed you got the ping? Head over to <#429079337132752907> and react with the logo to remove the Session Tag role!`, {embed: embed}).then(() => {
 		message.reply("Sent! Please make sure the next shift is **45 minutes** from this current shift.");
 		}).catch(error => {message.channel.send(`An unexpected error has occured. The developer has been sent the error message.`); bot.channels.get("707607323329429564").send(`An unexpected error has occured. ${message.author.tag} ran the shift command and an unexpected error occured: \n**${error}** \n*Error occured at ${timestamp}*`);
 });
