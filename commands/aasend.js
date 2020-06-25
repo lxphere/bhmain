5337afe76d01adb14e64abd93e394f084fab6df5
lxphere <47260747+lxphere@users.noreply.github.com>
2020-06-25 12:14:24 -0400

    Update aasend.js

--- commands/aasend.js
+++ commands/aasend.js
@@ -8,14 +8,14 @@ let member = message.mentions.members.first() || message.guild.members.get(args[
     let aapasser = new Discord.RichEmbed()
 		.setTitle("Bean House Administrative Assistant Applications - Batch 01 Passers")
 		.setColor("#85ffb0")
-		.setDescription("__Dear" + member + ",__"),
-    		fields: [{
+		.setDescription("__Dear" + member + ",__")
+    		.addFields({
 						name: "Information",
-						value: "We are extremely proud to announce that you have passed and accelerated in Bean House’s Administrative Assistant Applications! You will now be moving on to phase 2/2  which will consist of training and overall trials. This is to ensure you are fit for your role and can adequately execute what you propose within your application! We want to see the best of your capabilities and test them to the limits. Please do not feel overwhelmed as many other people will be undergoing Phase 2 with you! Once again, congratulations"
+						value: "We are extremely proud to announce that you have passed and accelerated in Bean House’s Administrative Assistant Applications! You will now be moving on to phase 2/2  which will consist of training and overall trials. This is to ensure you are fit for your role and can adequately execute what you propose within your application! We want to see the best of your capabilities and test them to the limits. Please do not feel overwhelmed as many other people will be undergoing Phase 2 with you! Once again, congratulations!"
 					},
 					{
 						name: "Expectations",
-						value: "**Before we commence Phase 2, it is expected you complete these requirements:\n** 
+						value: "**Before we commence Phase 2, it is expected you complete these requirements:\n**"
 						       + "> • Reply so we are aware you have received the response. (Do not send the bot a message, instead DM @samm#0021 (Lxphere)\n"
 						       + "> • Engage in shifts and portay activity.\n"
 						       + "> • Ensure you participate in your department and successfully complete any tasks given.\n"
@@ -26,8 +26,7 @@ let member = message.mentions.members.first() || message.guild.members.get(args[
 			 		{
 						name: "Extra",
 						value: "That is all you will be required to do, any extra info will be given in your respective department channel. \n**Best Regards,**\n**Lxphere**\n**Vice Chairman**"
-					}
-					]
+					})
 		.setFooter("Bean House Bot")
 		.setThumbnail("https://media.discordapp.net/attachments/528399681185906736/699372158870093925/image0.png?width=598&height=598");
     		await member.send({embed: aapasser})
